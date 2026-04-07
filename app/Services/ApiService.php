f47cac9e9a17b0646bc24fe98be00ad12159a6ce
IQ72 <faiq.fhr03@gmail.com>
2026-04-07 21:45:12 +0700

    Create ApiService.php

---
+++ app/Services/ApiService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class ApiService
+{
+    protected $token;
+
+    public function __construct()
+    {
+        $this->token = session('api_token');
+    }
+
+    protected function request($method, $endpoint, $data = [])
+    {
+        if (!$this->token) {
+            throw new \Exception('No API token in session');
+        }
+
+        $response = Http::withToken($this->token)->$method("http://localhost:8000/api/$endpoint", $data);
+
+        if ($response->status() == 401) {
+            session()->forget(['api_token', 'user_data']);
+            throw new \Exception('Unauthorized');
+        }
+
+        if (!$response->successful()) {
+            throw new \Exception('API request failed');
+        }
+
+        return $response->json()['data'] ?? [];
+    }
+
+    public function getBanners()
+    {
+        return $this->request('get', 'banners');
+    }
+
+    public function getServices()
+    {
+        return $this->request('get', 'layanan');
+    }
+}

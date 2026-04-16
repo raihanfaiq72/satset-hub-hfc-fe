80fa7860124b55a3dfb19176a17f985076d34a10
Emerio Kevin Aryaputra <emerio.kevin@gmail.com>
2026-04-16 09:06:16 +0700

    refactor: use component for profile editing

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -4,11 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SatSet - Dashboard Mockup</title>
+    <title>SatSet App</title>
     <link rel="icon" href="{{ asset('company-logo.png') }}" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'satset-green': '#2d7a6e',
+                        'satset-dark': '#246359'
+                    }
+                }
+            }
+        }
+    </script>
     @stack('script_head')
     @stack('style')
 </head>
